8a01640e8783d07dece61151c3292b30824290a2
TSeelig <thomas.seelig@HMCTS.NET>
2020-11-25 16:30:07 +0000

    RPET-269 remove generation of solicitor personal service letter

--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/IssuePersonalServicePackWorkflowTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/IssuePersonalServicePackWorkflowTest.java
@@ -6,16 +6,11 @@ import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import uk.gov.hmcts.reform.divorce.orchestration.domain.model.DocumentType;
-import uk.gov.hmcts.reform.divorce.orchestration.domain.model.LanguagePreference;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CaseDetails;
 import uk.gov.hmcts.reform.divorce.orchestration.domain.model.ccd.CcdCallbackRequest;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.WorkflowException;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.DefaultTaskContext;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskException;
-import uk.gov.hmcts.reform.divorce.orchestration.service.DocumentTemplateService;
-import uk.gov.hmcts.reform.divorce.orchestration.tasks.AddNewDocumentsToCaseDataTask;
-import uk.gov.hmcts.reform.divorce.orchestration.tasks.DocumentGenerationTask;
 import uk.gov.hmcts.reform.divorce.orchestration.tasks.PersonalServiceValidationTask;
 
 import java.util.Collections;
@@ -29,13 +24,7 @@ import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_CASE_ID;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.TEST_TOKEN;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AUTH_TOKEN_JSON_KEY;
-import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CASE_DETAILS_JSON_KEY;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CASE_ID_JSON_KEY;
-import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DOCUMENT_FILENAME;
-import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DOCUMENT_TEMPLATE_ID;
-import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.DOCUMENT_TYPE;
-import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.SOLICITOR_PERSONAL_SERVICE_LETTER_DOCUMENT_TYPE;
-import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.SOLICITOR_PERSONAL_SERVICE_LETTER_FILENAME;
 
 @RunWith(MockitoJUnitRunner.class)
 public class IssuePersonalServicePackWorkflowTest {
@@ -43,19 +32,8 @@ public class IssuePersonalServicePackWorkflowTest {
     @Mock
     PersonalServiceValidationTask personalServiceValidationTask;
 
-    @Mock
-    DocumentGenerationTask documentGenerationTask;
-
-    @Mock
-    AddNewDocumentsToCaseDataTask addNewDocumentsToCaseDataTask;
-
-    @Mock
-    private DocumentTemplateService documentTemplateService;
-
     @InjectMocks
     IssuePersonalServicePackWorkflow issuePersonalServicePackWorkflow;
-
-    private static final String SOLICITOR_PERSONAL_SERVICE_LETTER_TEMPLATE_ID = "FL-DIV-GNO-ENG-00073.docx";
 
     @Test
     public void testRunExecutesExpectedTasksInOrder() throws WorkflowException, TaskException {
@@ -66,19 +44,11 @@ public class IssuePersonalServicePackWorkflowTest {
                 .caseData(caseData)
                 .build();
 
-        when(documentTemplateService.getTemplateId(LanguagePreference.ENGLISH,
-                DocumentType.SOLICITOR_PERSONAL_SERVICE_LETTER_TEMPLATE_ID))
-                .thenReturn(SOLICITOR_PERSONAL_SERVICE_LETTER_TEMPLATE_ID);
-
         DefaultTaskContext context = new DefaultTaskContext();
         context.setTransientObjects(new HashMap<String, Object>() {
             {
                 put(AUTH_TOKEN_JSON_KEY, TEST_TOKEN);
                 put(CASE_ID_JSON_KEY, TEST_CASE_ID);
-                put(CASE_DETAILS_JSON_KEY, caseDetails);
-                put(DOCUMENT_TYPE, SOLICITOR_PERSONAL_SERVICE_LETTER_DOCUMENT_TYPE);
-                put(DOCUMENT_TEMPLATE_ID, SOLICITOR_PERSONAL_SERVICE_LETTER_TEMPLATE_ID);
-                put(DOCUMENT_FILENAME, SOLICITOR_PERSONAL_SERVICE_LETTER_FILENAME);
             }
         });
 
@@ -88,19 +58,14 @@ public class IssuePersonalServicePackWorkflowTest {
 
         //when
         when(personalServiceValidationTask.execute(context, caseData)).thenReturn(caseData);
-        when(documentGenerationTask.execute(context, caseData)).thenReturn(caseData);
-        when(addNewDocumentsToCaseDataTask.execute(context, caseData)).thenReturn(caseData);
+
         Map<String, Object> response = issuePersonalServicePackWorkflow.run(request, TEST_TOKEN);
 
         //then
         assertThat(response, is(caseData));
         InOrder inOrder = inOrder(
-                personalServiceValidationTask,
-                documentGenerationTask,
-            addNewDocumentsToCaseDataTask
+                personalServiceValidationTask
         );
         inOrder.verify(personalServiceValidationTask).execute(context, caseData);
-        inOrder.verify(documentGenerationTask).execute(context, caseData);
-        inOrder.verify(addNewDocumentsToCaseDataTask).execute(context, caseData);
     }
 }
